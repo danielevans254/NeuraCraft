dc993ce460643e50d87c2dbc227a41105a057b2b
Daniel Evans <danielevans254@yahoo.com>
2024-10-15 08:50:19 +0800

    Add feat: create getAllCourseNames API endpoint

---
+++ neuracraft/src/pages/api/forum/getAllCourseNames.ts
@@ -0,0 +1,20 @@
+import { NextApiRequest, NextApiResponse } from "next";
+
+import { prisma } from "@/server/db/client";
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const courseNames = await prisma.course.findMany({
+    select: {
+      courseSlug: true,
+      courseName: true,
+      courseLevel: true,
+      type: true,
+      topics: true,
+    },
+  });
+
+  res.status(200).json(courseNames);
+}
