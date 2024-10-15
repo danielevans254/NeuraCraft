3acec40b34c985d6e32222cd8413e590e5089b00
Daniel Evans <danielevans254@yahoo.com>
2024-10-15 08:57:26 +0800

    feat: Add client-side and server-side environment variable validation

---
+++ neuracraft/src/env/schema.mjs
@@ -0,0 +1,44 @@
+// @ts-check
+import { z } from "zod";
+
+/**
+ * Specify your server-side environment variables schema here.
+ * This way you can ensure the app isn't built with invalid env vars.
+ */
+export const serverSchema = z.object({
+  NODE_ENV: z.enum(["development", "production"]),
+  DATABASE_URL: z.string(),
+  CLOUDINARY_URL: z.string(),
+  CLOUDINARY_API_KEY: z.string(),
+  CLOUDINARY_SECRET: z.string(),
+  NEXTAUTH_SECRET: z.string(),
+  NEXTAUTH_URL: z.string().url(),
+  GOOGLE_CLIENT_ID: z.string(),
+  GOOGLE_CLIENT_SECRET: z.string(),
+  RECOMMENDER_URL: z.string(),
+  RECOMMENDER_API_KEY: z.string(),
+  GMAIL: z.string(),
+  GMAIL_PASS: z.string(),
+  EMAIL_SERVER: z.string(),
+  EMAIL_FROM: z.string(),
+  ACTION_KEY: z.string(),
+});
+
+/**
+ * Specify your client-side environment variables schema here.
+ * This way you can ensure the app isn't built with invalid env vars.
+ * To expose them to the client, prefix them with `NEXT_PUBLIC_`.
+ */
+export const clientSchema = z.object({
+  // NEXT_PUBLIC_BAR: z.string(),
+});
+
+/**
+ * You can't destruct `process.env` as a regular object, so you have to do
+ * it manually here. This is because Next.js evaluates this at build time,
+ * and only used environment variables are included in the build.
+ * @type {{ [k in keyof z.infer<typeof clientSchema>]: z.infer<typeof clientSchema>[k] | undefined }}
+ */
+export const clientEnv = {
+  // NEXT_PUBLIC_BAR: process.env.NEXT_PUBLIC_BAR,
+};
