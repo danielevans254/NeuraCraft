443b5b0d9983b3da59d33c3f342d643ffc79b228
Daniel Evans <danielevans254@yahoo.com>
2024-10-20 09:32:06 +0800

    feat: Add getAllTopicNames API endpoint

---
+++ neuracraft/src/pages/api/forum/getAllTopicNames.ts
@@ -0,0 +1,18 @@
+import { NextApiRequest, NextApiResponse } from "next";
+
+import { prisma } from "@/server/db/client";
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const posts = await prisma.topic.findMany({
+    select: {
+      topicName: true,
+      topicSlug: true,
+      topicLevel: true,
+    },
+  });
+
+  res.status(200).json(posts);
+}
