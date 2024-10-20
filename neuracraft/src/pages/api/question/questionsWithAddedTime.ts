a952716491d15fb4eb6a7165d22e786a6e896aae
Daniel Evans <danielevans254@yahoo.com>
2024-10-20 09:32:33 +0800

    feat: Add API endpoint for retrieving questions with added time

---
+++ neuracraft/src/pages/api/question/questionsWithAddedTime.ts
@@ -0,0 +1,82 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import { getServerSession } from "next-auth/next";
+
+import { authOptions } from "@/pages/api/auth/[...nextauth]";
+import { prisma } from "@/server/db/client";
+import { QuestionDataType } from "@/types/question-types";
+import { CustomEval } from "@/utils/CustomEval";
+import { CustomMath } from "@/utils/CustomMath";
+import { RecommendQuestion } from "@/utils/Recommender";
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const session = await getServerSession(req, res, authOptions);
+
+  // Questions specific to user and course, newest first
+  let userCourseQuestionsWithAddedTime =
+    await prisma.questionWithAddedTime.findFirst({
+      where: {
+        userId: session?.user?.id,
+        courseSlug: req.query.courseSlug as string,
+      },
+      include: {
+        question: {
+          include: {
+            topic: {
+              select: {
+                topicName: true,
+              },
+            },
+          },
+        },
+      },
+      orderBy: {
+        addedTime: "desc",
+      },
+    });
+
+  // If no questions for this user and for this course yet, recommend a question from the course
+  if (!userCourseQuestionsWithAddedTime) {
+    const { recommendedTopicName, recommendedQuestion } =
+      await RecommendQuestion(req.query.courseSlug as string, 0);
+
+    const questionData = recommendedQuestion.questionData as QuestionDataType;
+
+    let evaluatedQuestionData;
+    if (recommendedQuestion.variationId === 0) {
+      evaluatedQuestionData = CustomEval(
+        questionData.variables,
+        questionData.methods
+      );
+    }
+
+    const recommendedQuestionsWithAddedTime =
+      await prisma.questionWithAddedTime.create({
+        data: {
+          userId: session?.user?.id as string,
+          courseSlug: req.query.courseSlug as string,
+          questionId: recommendedQuestion.questionId,
+          variationId: recommendedQuestion.variationId,
+          variables:
+            evaluatedQuestionData?.questionVariables ?? questionData.variables,
+          answers: CustomMath.shuffleArray(
+            questionData.answers ?? evaluatedQuestionData?.questionAnswers
+          ) as QuestionDataType["answers"],
+        },
+      });
+
+    userCourseQuestionsWithAddedTime = {
+      ...recommendedQuestionsWithAddedTime,
+      question: {
+        ...recommendedQuestion,
+        topic: {
+          topicName: recommendedTopicName,
+        },
+      },
+    };
+  }
+
+  res.status(200).json(userCourseQuestionsWithAddedTime);
+}
