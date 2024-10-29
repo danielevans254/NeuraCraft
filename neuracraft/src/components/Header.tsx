ba796312aa75496ecadbef27b0648fd15e5b08a4
Daniel Evans <danielevans254@yahoo.com>
2024-10-29 08:19:45 +0800

    refactor: Update login streak

--- neuracraft/src/components/Header.tsx
+++ neuracraft/src/components/Header.tsx
@@ -49,7 +49,7 @@ const Header = ({ title = "Personalized  Adaptive ELearning Platform" }) => {
           customIcon: "📅",
           message: (
             <>
-              Login Streak: {res?.data?.loginStreak} 🔋
+              Login Streak: {res?.data?.loginStreak} 🪙
               <span className="text-yellow-600">
                 +{res?.data?.loginStreak < 5 ? res?.data?.loginStreak : 5}
               </span>
